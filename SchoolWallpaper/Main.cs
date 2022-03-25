4ac3c2034f9ca8f773b8a920fad63ad3ab25a6d3
Banana0910 <dleogus0910@gmail.com>
2022-03-25 15:36:34 +0900

    03/25 commit

--- SchoolWallpaper/Main.cs
+++ SchoolWallpaper/Main.cs
@@ -26,14 +26,44 @@ namespace SchoolWallpaper
             InitializeComponent();
         }
 
-        string[] times = new string[] { 
-            "(08:35 ~ 08:40)", "(08:40 ~ 09:30)",
-            "(09:30 ~ 09:40)", "(09:40 ~ 10:30)",
-            "(10:30 ~ 10:40)", "(10:40 ~ 11:30)",
-            "(11:30 ~ 11:40)", "(11:40 ~ 12:30)",
-            "(12:30 ~ 13:30)", "(13:30 ~ 14:20)",
-            "(14:20 ~ 14:30)", "(14:30 ~ 15:20)",
-            "(15:20 ~ 15:35)", "(15:35 ~ 16:25)"
+        // string[] starttimes = new string[] { 
+        //     "(08:35 ~ 08:40)", "(08:40 ~ 09:30)",
+        //     "(09:30 ~ 09:40)", "(09:40 ~ 10:30)",
+        //     "(10:30 ~ 10:40)", "(10:40 ~ 11:30)",
+        //     "(11:30 ~ 11:40)", "(11:40 ~ 12:30)",
+        //     "(12:30 ~ 13:30)", "(13:30 ~ 14:20)",
+        //     "(14:20 ~ 14:30)", "(14:30 ~ 15:20)",
+        //     "(15:20 ~ 15:35)", "(15:35 ~ 16:25)"
+        // };
+
+        string[] times = new string[] {
+            "08:35", "08:40", "09:30", "09:40", "10:30", "10:40"," 11:30", "11:40","12:30", "13:30", "14:20", "14:30", "15:20", "15:35", "16:25"
+        };
+
+        // 1교시 : lesson = 1, times[lesson] ~ times[lesson+1], lesson = 2, times[lesson+2], times[lesson+2+1], lesson = 3
+        // 0 : 월, 1 : 화, 2 : 수, 3 : 목, 4 : 금
+        string[,,] timetables = new string[,,] {
+            { 
+                { "한문", "수학", "과학탐구", "체육", "영어", "통합과학", "국어"},
+                { "송윤정", "배주경", "문정원", "박효열", "이은진", "백분이", "임양경"}
+            },
+            { 
+                { "한국사", "수학", "한국사", "국어", "통합사회", "진로", "영어"},
+                { "이현선", "김석룡", "하상억", "장미정", "장봉선", "김화정", "정주은"}
+            },
+            { 
+                {"통합사회", "미술", "미술", "자율", "창체", "창체", null},
+                {"김현우", "김영은", "김영은", "송윤정", "장미정", "장미정", null}
+            },
+            { 
+                {"체육", "영어", "수학", "통합과학", "국어", "기가", "기가"},
+                {"박효열", "이은진", "배주경", "김수영", "장미정", "서현희", "서현희"}
+            },
+            { 
+                {"국어", "한국사", "영어", "통합사회", "통합과학", "수학", "한문"},
+                {"임양경", "하상억", "정주은", "김현우", "정송은", "김석룡", "송윤정"}
+            },
+
         };
 
         string backup_wallpaper = "";
@@ -54,7 +84,7 @@ namespace SchoolWallpaper
             WebClient wc = new WebClient();
             wc.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11";
             wc.QueryString.Add("dietDate", DateTime.Now.ToString("yyyy/MM/dd"));
-            wc.Encoding = Encoding.UTF8;
+            wc.Encoding = Encoding. UTF8;
             string html = wc.DownloadString("http://bongrim-h.gne.go.kr/bongrim-h/dv/dietView/selectDietDetailView.do");
 
             hap.HtmlDocument htmlDoc = new hap.HtmlDocument();
@@ -77,7 +107,7 @@ namespace SchoolWallpaper
             // checker.Interval = 1000;
             // checker.Start();
             backup_wallpaper = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop").GetValue("WallPaper").ToString();
-            set_break(1);
+            set_subject(7, "한문", "송윤정");
         }
 
         private void set_subject(int lesson, string subject, string teacher) {
@@ -124,10 +154,12 @@ namespace SchoolWallpaper
             Font teacher_name = new Font("나눔스퀘어 ExtraBold", 35);
             Font lesson_str = new Font("나눔스퀘어 Bold", 35);
             Font time_str = new Font("나눔고딕", 30);
+
+            string time = $"{times[lesson*2-1]} ~ {times[lesson*2]}";
             
             int center_y = (class_y_bar.Maximum - class_y_bar.Value) - ((TextRenderer.MeasureText(subject, subject_title).Height + 
                 TextRenderer.MeasureText(teacher, teacher_name).Height + 
-                TextRenderer.MeasureText(times[lesson*2+1], time_str).Height) / 2);
+                TextRenderer.MeasureText(time, time_str).Height) / 2);
             int center_x = class_x_bar.Value - (TextRenderer.MeasureText($"{lesson}교시", lesson_str).Width / 2);
 
             g.DrawString($"{lesson}교시", lesson_str, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
@@ -141,9 +173,9 @@ namespace SchoolWallpaper
 
             g.DrawString(teacher, teacher_name, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
             center_y += TextRenderer.MeasureText(teacher, teacher_name).Height + 10;
-            center_x = class_x_bar.Value - (TextRenderer.MeasureText(times[lesson*2+1], time_str).Width / 2);
+            center_x = class_x_bar.Value - (TextRenderer.MeasureText(time, time_str).Width / 2);
 
-            g.DrawString(times[lesson*2+1], time_str, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
+            g.DrawString(time, time_str, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
 
             //저장 및 적용
             image.Save("wallpaper.png", System.Drawing.Imaging.ImageFormat.Png);
@@ -196,8 +228,10 @@ namespace SchoolWallpaper
             Font lesson_str = new Font("나눔스퀘어 Bold", 35);
             Font time_str = new Font("나눔고딕", 30);
             
+            string time = $"{times[lesson*2-2]} ~ {times[lesson*2-1]}";
+            
             int center_y = (class_y_bar.Maximum - class_y_bar.Value) - ((TextRenderer.MeasureText("쉬는 시간", subject_title).Height + 
-                TextRenderer.MeasureText(times[lesson*2+1], time_str).Height) / 2);
+                TextRenderer.MeasureText(time, time_str).Height) / 2);
             int center_x = class_x_bar.Value - (TextRenderer.MeasureText("Next 영어(정주은)", lesson_str).Width / 2);
 
             g.DrawString($"Next 영어(정주은)", lesson_str, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
@@ -205,10 +239,10 @@ namespace SchoolWallpaper
             center_y += TextRenderer.MeasureText($"{lesson}교시", lesson_str).Height;
 
             g.DrawString("쉬는 시간", subject_title, color, new RectangleF(center_x,center_y,image.Width,image.Height));
-            center_x = class_x_bar.Value - (TextRenderer.MeasureText(times[lesson*2], time_str).Width / 2);
+            center_x = class_x_bar.Value - (TextRenderer.MeasureText(time, time_str).Width / 2);
             center_y += TextRenderer.MeasureText("쉬는 시간", subject_title).Height;
 
-            g.DrawString(times[lesson*2], time_str, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
+            g.DrawString(time, time_str, tiny_color, new RectangleF(center_x,center_y,image.Width,image.Height));
 
             //저장 및 적용
             image.Save("wallpaper.png", System.Drawing.Imaging.ImageFormat.Png);
@@ -233,68 +267,57 @@ namespace SchoolWallpaper
         private void Main_Load(object sender, EventArgs e)
         {
             class_x_bar.Maximum = Screen.PrimaryScreen.Bounds.Width;
+            class_x_bar.Value = class_x_bar.Maximum / 2;
+
             class_y_bar.Maximum = Screen.PrimaryScreen.Bounds.Height;
+            class_y_bar.Value = class_y_bar.Maximum / 2;
+
             date_x_bar.Maximum = Screen.PrimaryScreen.Bounds.Width;
+            date_x_bar.Value = date_x_bar.Maximum / 2;
             font_color_box.BackColor = Color.FromArgb(255, 0, 0, 0);
+        }
+
+        private bool in_time(string start, string end) {
+            return DateTime.Parse(start) >= DateTime.Now && DateTime.Parse(end) < DateTime.Now;
         }
 
         private void checker_Tick(object sender, EventArgs e)
         {
-            if (DateTime.Parse("08:30:00") < DateTime.Now) {
+            DateTime now = DateTime.Now;
+            if (DateTime.Parse("08:30:00") < now) {
                 //아침시간
                 return;
-            }
+            } else if (in_time(times[0], times[1])) {
 
-            switch (DateTime.Now.ToString("HH:mm:ss")) {
-                case "08:30:00" :
-                    // 조례
-                    break;
-                case "08:35:00" :
-                    // 쉬는 시간
-                    break;
-                case "08:40:00" :
-                    // 1교시
-                    break;
-                case "09:30:00" :
-                    // 쉬는 시간
-                    break;
-                case "09:40:00" :
-                    // 2교시
-                    break;
-                case "10:30:00" :
-                    // 쉬는 시간
-                    break;
-                case "10:40:00" :
-                    // 3교시
-                    break;
-                case "11:30:00" :
-                    // 쉬는 시간
-                    break;
-                case "11:40:00" :
-                    // 4교시
-                    break;
-                case "12:30:00" :
-                    //점심 시간
-                    break;
-                case "13:30:00" :
-                    //5교시
-                    break;
-                case "14:20:00" :
-                    // 쉬는 시간
-                    break;
-                case "14:30:00" :
-                    // 6교시
-                    break;
-                case "15:20:00" :
-                    // 청소 시간
-                    break;
-                case "15:35:00" :
-                    // 7교시
-                    break;
-                case "16:25:00" :
-                    // 종례
-                    break;
-            }
+            } else if (in_time(times[1], times[2])) {
+
+            } else if (in_time(times[2], times[3])) { 
+
+            } else if (in_time(times[2], times[3])) { 
+
+            } else if (in_time(times[3], times[4])) { 
+
+            } else if (in_time(times[4], times[5])) { 
+
+            } else if (in_time(times[5], times[6])) { 
+
+            } else if (in_time(times[6], times[7])) { 
+
+            } else if (in_time(times[7], times[8])) { 
+
+            } else if (in_time(times[8], times[9])) { 
+
+            } else if (in_time(times[9], times[10])) { 
+
+            } else if (in_time(times[10], times[11])) { 
+
+            } else if (in_time(times[11], times[12])) { 
+
+            } else if (in_time(times[12], times[13])) { 
+
+            } else if (in_time(times[13], times[14])) { 
+
+            } 
         }
 
         private void Main_FormClosed(object sender, FormClosedEventArgs e)
